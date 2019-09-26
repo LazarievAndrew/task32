03beb6c57d7ff3da74be6d8e9350b32f216d16ab
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-09-27 02:22:44 +0300

    eslint-config-airbnb-base

--- src/scripts/createList.js
+++ src/scripts/createList.js
@@ -1,32 +1,32 @@
-import listOnClick from './listOnClick';
+// import listOnClick from './listOnClick';
 
-export default function createList(dogs, className, addingClassName, creatingElement) {
-    const fragment = document.createDocumentFragment();
-    const element = document.createElement(creatingElement);
-    element.classList.add(addingClassName);
+// export default function createList(dogs, className, addingClassName, creatingElement) {
+//     const fragment = document.createDocumentFragment();
+//     const element = document.createElement(creatingElement);
+//     element.classList.add(addingClassName);
 
-    for (let i = 0; i < dogs.length; i++) {
-        const clone = element.cloneNode();
-        clone.textContent = dogs[i];
-        element.classList.add(addingClassName);
-        fragment.appendChild(clone);
-        listOnClick(clone, dogs[i]);
-    }
+//     for (let i = 0; i < dogs.length; i++) {
+//         const clone = element.cloneNode();
+//         clone.textContent = dogs[i];
+//         element.classList.add(addingClassName);
+//         fragment.appendChild(clone);
+//         listOnClick(clone, dogs[i]);
+//     }
 
-    // for (dog of dogs) {                    // webpack не пропускает, дает ошибку
-    //     let clone = element.cloneNode();
-    //     clone.textContent = dog;
-    //     element.classList.add(addingClassName);
-    //     fragment.appendChild(clone);
-    //     listOnClick(clone, dog)
-    // }
+//     // for (dog of dogs) {                    // webpack не пропускает, дает ошибку
+//     //     let clone = element.cloneNode();
+//     //     clone.textContent = dog;
+//     //     element.classList.add(addingClassName);
+//     //     fragment.appendChild(clone);
+//     //     listOnClick(clone, dog)
+//     // }
 
-    const parent = document.getElementsByClassName(className)[0];
-    if (creatingElement === 'option') {
-        element.setAttribute('disabled', '');
-        element.setAttribute('selected', '');
-        element.textContent = 'подпорода';
-        parent.appendChild(element);
-    }
-    parent.appendChild(fragment);
-}
+//     const parent = document.getElementsByClassName(className)[0];
+//     if (creatingElement === 'option') {
+//         element.setAttribute('disabled', '');
+//         element.setAttribute('selected', '');
+//         element.textContent = 'подпорода';
+//         parent.appendChild(element);
+//     }
+//     parent.appendChild(fragment);
+// }
--- src/scripts/listOnClick.js
+++ src/scripts/listOnClick.js
@@ -11,3 +11,35 @@ export default function listOnClick(element, breed) {
             .then(() => createTitle(breed, '.title'));
     };
 }
+
+function createList(dogs, className, addingClassName, creatingElement) {
+    const fragment = document.createDocumentFragment();
+    const element = document.createElement(creatingElement);
+    element.classList.add(addingClassName);
+
+    for (let i = 0; i < dogs.length; i++) {
+        const clone = element.cloneNode();
+        clone.textContent = dogs[i];
+        element.classList.add(addingClassName);
+        fragment.appendChild(clone);
+        listOnClick(clone, dogs[i]);
+    }
+
+    // for (dog of dogs) {                    // webpack не пропускает, дает ошибку
+    //     let clone = element.cloneNode();
+    //     clone.textContent = dog;
+    //     element.classList.add(addingClassName);
+    //     fragment.appendChild(clone);
+    //     listOnClick(clone, dog)
+    // }
+
+    const parent = document.getElementsByClassName(className)[0];
+    if (creatingElement === 'option') {
+        element.setAttribute('disabled', '');
+        element.setAttribute('selected', '');
+        element.textContent = 'подпорода';
+        parent.appendChild(element);
+    }
+    parent.appendChild(fragment);
+}
+export { createList };
